30de2e19195600f749d521675980db64ae836181
Diego Campos <diego.campos@zarpsystem.com.br>
2017-11-24 11:44:45 -0200

    Inicializacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  @Input() slideId = 1;
+
+  avancar() {
+    this.slideId++;
+  }
+
+  voltar() {
+    this.slideId--;
+  }
 }
